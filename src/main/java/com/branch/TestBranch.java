d9a5748657ff987d5437ab5e062bf6d225e3e6df
yangdaobin <yangdaobin@jcfc.cn>
2018-09-29 14:18:32 +0800

    dev1 commit3
    dev1 commit4

--- src/main/java/com/branch/TestBranch.java
+++ src/main/java/com/branch/TestBranch.java
@@ -7,6 +7,8 @@ public class TestBranch {
     public void test() {
         System.out.println(1);
         System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
     }
 
 }
